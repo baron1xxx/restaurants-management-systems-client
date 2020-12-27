fdeea2becafc891c5ee7e583c2d04e8c3bcb6374
rommyk <baron1xxx@gmail.com>
2020-12-27 23:12:05 +0200

    Add core module

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,10 +1,17 @@
-import {NgModule} from '@angular/core';
+import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {throwIfAlreadyLoaded} from './guard/module-import.guard';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}
